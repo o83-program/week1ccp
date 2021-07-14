62eac1a02955ddb283e22e29ff7c1e5234203e52
o83-program <84218325+o83-program@users.noreply.github.com>
2021-07-14 18:15:22 +0530

    Update RANGE.c

--- RANGE.c
+++ RANGE.c
@@ -8,7 +8,7 @@
     scanf("%d",&m);
     printf("\n enter the value of n");
     scanf("%d",&n);
-    for(i=m;i<=n;i++)
+    for(i=1;i<=10;i++)
 {
     if(i%2==0)
     printf("\n it is even number=%d",i);
